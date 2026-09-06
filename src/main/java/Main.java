1d0b5ca386b3dc85b52e7ce33aab2aa7b95a9a20
deoksisss <thesuperden.yttv@yandex.ru>
2026-09-06 19:56:28 +0500

    calculator добавлен

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,53 @@ import java.util.List;
 
 public class Main {
 
-    public static int calculator(String expression) {
+    public static double calculator(String expression) {
+        List<Double> numbers = new ArrayList<>();
+        List<Character> operators = new ArrayList<>();
 
+        // Парсинг строки
+        StringBuilder sb = new StringBuilder(); // буфер для записи числа
+        for (char c : expression.toCharArray()) {
+            if ("+-*/".indexOf(c) != -1) { //Проверка, являетс ли текущий элемент строки знаком
+                numbers.add(Double.parseDouble(sb.toString())); // добавление буфера в спсиок чисел
+                operators.add(c); // добавление самого знака в список знаков
+                sb.setLength(0); // чистка буфера
+            } else {
+                sb.append(c); // добавление новой чифры в буфера
+            }
+        }
+        numbers.add(Double.parseDouble(sb.toString()));
 
+        // выполнение умножения и деления
+        for (int i = 0; i < operators.size(); i++) {
+            char op = operators.get(i);
+            if (op == '*' || op == '/') {
+                double left = numbers.get(i);
+                double right = numbers.get(i + 1);
+
+                if (op == '/' && right == 0) {
+                    return Double.NaN; // при наличии деления на ноль calculator досрочно завершает работу
+                }
+
+                double res = (op == '*') ? (left * right) : (left / right);
+
+                numbers.set(i, res);
+                numbers.remove(i + 1);
+                operators.remove(i);
+                i--; // Смещение индекса назад после удаления числа
+            }
+        }
+
+        // выполнение сложения и вычитания
+        double result = numbers.get(0); // переход из List<Double> к double
+        for (int i = 0; i < operators.size(); i++) {
+            char op = operators.get(i);
+            double next = numbers.get(i + 1);
+            if (op == '+') result += next;
+            if (op == '-') result -= next;
+        }
+
+        return result;
     }
 
     public static List<String> validator(List<String> expressions) {
